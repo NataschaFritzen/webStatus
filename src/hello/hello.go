7799e47ededece58904dd562bcfd7857698f9229
Natascha Fritzen <119437922+NataschaFritzen@users.noreply.github.com>
2025-04-14 15:29:48 -0300

    primeiro commit

---
+++ src/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nome string = "Joana"
+	var versao float32 = 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
